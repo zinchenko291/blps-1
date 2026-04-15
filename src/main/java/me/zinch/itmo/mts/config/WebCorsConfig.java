9be33805c4a1ac6090f7884f84464477374ff7af
Ivan Zinchenko <i@zinch.me>
2026-04-15 19:25:17 +0300

    cors

--- src/main/java/me/zinch/itmo/mts/config/WebCorsConfig.java
+++ src/main/java/me/zinch/itmo/mts/config/WebCorsConfig.java
@@ -1,5 +1,6 @@
 package me.zinch.itmo.mts.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,12 +8,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebCorsConfig implements WebMvcConfigurer {
 
+    @Value("${app.web.cors}")
+    private boolean appWebCors;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:3001")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+        if (appWebCors) {
+            registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:3000", "http://localhost:3001")
+                    .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+        }
     }
 }
